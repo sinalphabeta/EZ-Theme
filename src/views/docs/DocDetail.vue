bb9d6562107636952dea16b6b4ba55d58c09a142
sinalphabeta <53267553+sinalphabeta@users.noreply.github.com>
2025-10-24 22:27:22 +0800

    feat: add some styles

--- src/views/docs/DocDetail.vue
+++ src/views/docs/DocDetail.vue
@@ -2135,6 +2135,8 @@ onUnmounted(() => {
 <style lang="scss">
 @use '@/assets/styles/no-plan-card' as *;
 
+@import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css');
+
 
 
 .eztheme-btn {
